497c3c415d37a9c5577d8c5ae5ea18f5c95725cd
Peita Lin <n6378056@gmail.com>
2017-04-30 17:58:02 +1000

    router fader: CSSTransitionGroup wrapper for react router

--- src/AppRoutes.tsx
+++ src/AppRoutes.tsx
@@ -4,8 +4,7 @@ import * as React from 'react'
 //// Routing
 import { BrowserRouter, HashRouter, Route, NavLink } from 'react-router-dom'
 import * as CSSTransitionGroup from 'react-transition-group/CSSTransitionGroup'
-
-
+import 'styles/AppRoutes.scss'
 //// antd
 import * as enUS from 'antd/lib/locale-provider/en_US';
 // import * as ruRU from 'antd/lib/locale-provider/ru_RU';
@@ -14,33 +13,34 @@ import * as LocaleProvider from 'antd/lib/locale-provider'
 import { lazyLoad } from './utils/lazyLoad'
 
 //// Components
-import Title from './components/Title'
-import 'styles/AppRoutes.scss'
-import LandingPage from './components/LandingPage'
-import LoginAuth0 from './components/LoginAuth0'
-import Navbar from './components/Navbar'
+// import Title from './components/Title'
+// import LandingPage from './components/LandingPage'
+// import LoginAuth0 from './components/LoginAuth0'
+// import Navbar from './components/Navbar'
+//
+// import MapSubscriptions from './components/MapSubscriptions'
+// import LocalPredictions from './components/LocalPredictions'
+// import PredictionListings from './components/PredictionListings'
+// import PredictionStats from './components/PredictionStats'
 
-import MapSubscriptions from './components/MapSubscriptions'
-import LocalPredictions from './components/LocalPredictions'
-import PredictionListings from './components/PredictionListings'
-import PredictionStats from './components/PredictionStats'
-
-import Demo from './components/DraggableGrid'
-import CardExpander from './components/CardExpander'
-
+// import DraggableGrid from './components/DraggableGrid'
+// import CardExpander from './components/CardExpander'
 
 
 //////// Lazy-loading Components by Route /////////
-// const Title = lazyLoad(() => System.import('./components/Title.tsx').then(module => module.default))
-// const LandingPage = lazyLoad(() => System.import('./components/LandingPage.tsx').then(module => module.default))
-// const LoginAuth0 = lazyLoad(() => System.import('./components/LoginAuth0.tsx').then(module => module.default))
-//
-// const PredictionListings = lazyLoad(() => System.import('./components/PredictionListings.tsx').then(module => module.default))
-// const PredictionStats = lazyLoad(() => System.import('./components/PredictionStats.tsx').then(module => module.default))
-//
-// const MapBackground = lazyLoad(() => System.import('./components/MapBackground.tsx').then(module => module.default))
-// const HouseStats = lazyLoad(() => System.import('./components/HouseStats.tsx').then(module => module.default))
-// const Navbar = lazyLoad(() => System.import('./components/Navbar.tsx').then(module => module.default))
+const Title = lazyLoad(() => System.import('./components/Title.tsx').then(module => module.default))
+const LandingPage = lazyLoad(() => System.import('./components/LandingPage.tsx').then(module => module.default))
+const LoginAuth0 = lazyLoad(() => System.import('./components/LoginAuth0.tsx').then(module => module.default))
+const Navbar = lazyLoad(() => System.import('./components/Navbar.tsx').then(module => module.default))
+
+const MapSubscriptions = lazyLoad(() => System.import('./components/MapSubscriptions.tsx').then(module => module.default))
+const LocalPredictions = lazyLoad(() => System.import('./components/LocalPredictions.tsx').then(module => module.default))
+const PredictionListings = lazyLoad(() => System.import('./components/PredictionListings.tsx').then(module => module.default))
+const PredictionStats = lazyLoad(() => System.import('./components/PredictionStats.tsx').then(module => module.default))
+
+const DraggableGrid = lazyLoad(() => System.import('./components/DraggableGrid.tsx').then(module => module.default))
+const CardExpander = lazyLoad(() => System.import('./components/CardExpander.tsx').then(module => module.default))
+
 
 
 const Login = (): JSX.Element => {
@@ -50,18 +50,17 @@ const Login = (): JSX.Element => {
   return <LoginAuth0 clientId={clientId} domain={domain} redirectOnAuth={redirectUrl}/>
 }
 
-const RouterFader = (WrappedComponent) => {
-    const TransitionedComponent = (props) => (
-      <CSSTransitionGroup
-        transitionAppear={true}
-        transitionAppearTimeout={600}
-        transitionEnterTimeout={600}
-        transitionLeaveTimeout={200}
-        transitionName="router-fade">
-        <WrappedComponent {...props} />
-      </CSSTransitionGroup>
-    );
-    return TransitionedComponent;
+const RouterFader = ( WrappedComponent ) => {
+  return (props) => (
+    <CSSTransitionGroup
+      transitionAppear={true}
+      transitionAppearTimeout={600}
+      transitionEnterTimeout={600}
+      transitionLeaveTimeout={200}
+      transitionName="router-fade">
+      <WrappedComponent {...props} />
+    </CSSTransitionGroup>
+  );
 };
 
 
@@ -82,7 +81,7 @@ export default class AppRoutes extends React.Component {
             <Route path="/map/predictionlistings/:id" component={ RouterFader(PredictionStats) } />
 
             <Route path="/test" component={ RouterFader(CardExpander) } />
-            <Route path="/profile" component={ RouterFader(Demo) } />
+            <Route path="/profile" component={ RouterFader(DraggableGrid) } />
           </div>
         </HashRouter>
       </LocaleProvider>
