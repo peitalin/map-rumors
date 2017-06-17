c9eda4d83fe8c0d9b473a81259d20e63a59b9625
Peita Lin <n6378056@gmail.com>
2017-06-17 18:23:12 +1000

    test iframe embeds

--- src/components/LocalPredictions.tsx
+++ src/components/LocalPredictions.tsx
@@ -100,6 +100,9 @@ export class LocalPredictions extends React.Component<DispatchProps & StateProps
             <br/>
             <div><h2>SUBURBIA INFOGRATIS</h2></div>
             <br/>
+            <iframe src="https://www.domain.com.au/suburb-profile/algester-qld-4115" width="100%" height="500">
+              <p>Your browser does not support iframes.</p>
+            </iframe>
             <div> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
             </div>
             <br/>
